454973dda6bf3375f62302f2b4e65be2ff9af3cf
XixianLiang <51265902107@stu.ecnu.edu.cn>
2025-06-04 12:27:47 +0800

    update monkeyq.jar and build.sh

--- build_monkeyq.sh
+++ build_monkeyq.sh
@@ -1,4 +1,5 @@
 #!/bin/zsh
 
+
 ./gradlew clean makeJar
-~/Library/Android/sdk/build-tools/28.0.3/dx --dex --min-sdk-version=26 --output=monkeyq.jar ~/Desktop/coding/Fastbot_Android/monkey/build/libs/monkey.jar
+$ANDROID_HOME/build-tools/28.0.3/dx --dex --min-sdk-version=26 --output=monkeyq.jar $(pwd)/monkey/build/libs/monkey.jar
